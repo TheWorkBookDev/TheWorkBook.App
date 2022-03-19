e5454d82e9d8aa060042f37b19b02b8d644b5dc6
Ronan Farrell <ronan.farrell@j2.com>
2022-03-19 23:41:37 +0000

    Setting up DI & some auth classes

---
+++ src/TheWorkBook/AccessTokenHttpMessageHandler.cs
@@ -0,0 +1,63 @@
+﻿using IdentityModel.Client;
+using IdentityModel.OidcClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheWorkBook
+{
+    public class AccessTokenHttpMessageHandler : DelegatingHandler
+    {
+        protected OidcClient OidcClient { get; }
+
+        public AccessTokenHttpMessageHandler(OidcClient oidcClient) : base(new HttpClientHandler())
+        {
+            OidcClient = oidcClient;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            string accessToken = await SecureStorage.GetAsync(OidcConsts.AccessTokenKeyName);
+
+            if (!string.IsNullOrWhiteSpace(accessToken))
+            {
+                request.SetBearerToken(accessToken);
+                request.Headers.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            }
+
+            var response = await base.SendAsync(request, cancellationToken);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                string refreshToken = await SecureStorage.GetAsync(OidcConsts.RefreshTokenKeyName);
+                if (!string.IsNullOrWhiteSpace(refreshToken))
+                {
+                    var refreshResult = await OidcClient.RefreshTokenAsync(refreshToken);
+
+                    await SecureStorage.SetAsync(OidcConsts.AccessTokenKeyName, refreshResult.AccessToken);
+                    await SecureStorage.SetAsync(OidcConsts.RefreshTokenKeyName, refreshResult.RefreshToken);
+
+                    request.SetBearerToken(refreshResult.AccessToken);
+
+                    return await base.SendAsync(request, cancellationToken);
+                }
+                else
+                {
+                    var result = await OidcClient.LoginAsync(new LoginRequest());
+                    request.SetBearerToken(result.AccessToken);
+
+                    await SecureStorage.SetAsync(OidcConsts.AccessTokenKeyName, result.AccessToken);
+                    await SecureStorage.SetAsync(OidcConsts.RefreshTokenKeyName, result.RefreshToken);
+
+                    request.SetBearerToken(result.AccessToken);
+
+                    return await base.SendAsync(request, cancellationToken);
+                }
+            }
+
+            return response;
+        }
+    }
+}
--- src/TheWorkBook/MauiProgram.cs
+++ src/TheWorkBook/MauiProgram.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+﻿using IdentityModel.OidcClient;
+using Microsoft.AspNetCore.Components.WebView.Maui;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
+using System.Reflection;
 using TheWorkBook.Data;
 
 namespace TheWorkBook;
@@ -16,9 +20,40 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 			});
 
+		ConfigureConfiguration(builder);
+
 		builder.Services.AddBlazorWebView();
 		builder.Services.AddSingleton<WeatherForecastService>();
 
+		builder.Services.AddTransient<WebAuthenticatorBrowser>();
+		builder.Services.AddTransient<MainPage>();
+
+		List<string> loopbackAddresses = new()
+		{
+			"127.0.0.1",
+			"192.168.1.5"
+		};
+
+		builder.Services.AddTransient<OidcClient>(sp =>
+			new OidcClient(new OidcClientOptions
+			{
+				Authority = "http://192.168.1.5:5001",
+				ClientId = "native",
+				RedirectUri = "theworkbook://authorize",
+				Scope = "openid profile api offline_access",
+				Browser = sp.GetRequiredService<WebAuthenticatorBrowser>(),
+
+				// When locally debugging
+				Policy = new Policy() { Discovery = new IdentityModel.Client.DiscoveryPolicy() { RequireHttps = false, LoopbackAddresses = loopbackAddresses } },
+			})
+		);
+
 		return builder.Build();
+	}
+
+	private static void ConfigureConfiguration(MauiAppBuilder builder)
+	{
+		var assembly = typeof(App).GetTypeInfo().Assembly;
+		builder.Configuration.AddJsonFile(new EmbeddedFileProvider(assembly), "appsettings.json", optional: false, false);
 	}
 }
