5931ca93989ba09e3fafe171b8e9355b6fdce1f8
Ronan Farrell <ronan.farrell@j2.com>
2022-03-20 00:04:55 +0000

    Auth config for production.

--- src/TheWorkBook/MauiProgram.cs
+++ src/TheWorkBook/MauiProgram.cs
@@ -9,51 +9,63 @@ namespace TheWorkBook;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.RegisterBlazorMauiWebView()
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .RegisterBlazorMauiWebView()
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
 
-		ConfigureConfiguration(builder);
+        ConfigureConfiguration(builder);
 
-		builder.Services.AddBlazorWebView();
-		builder.Services.AddSingleton<WeatherForecastService>();
+        builder.Services.AddBlazorWebView();
+        builder.Services.AddSingleton<WeatherForecastService>();
 
-		builder.Services.AddTransient<WebAuthenticatorBrowser>();
-		builder.Services.AddTransient<MainPage>();
+        builder.Services.AddTransient<WebAuthenticatorBrowser>();
+        builder.Services.AddTransient<MainPage>();
 
-		List<string> loopbackAddresses = new()
-		{
-			"127.0.0.1",
-			"192.168.1.5"
-		};
+        List<string> loopbackAddresses = new()
+        {
+            "127.0.0.1",
+            "192.168.1.5"
+        };
 
-		builder.Services.AddTransient<OidcClient>(sp =>
-			new OidcClient(new OidcClientOptions
-			{
-				Authority = "http://192.168.1.5:5001",
-				ClientId = "native",
-				RedirectUri = "theworkbook://authorize",
-				Scope = "openid profile api offline_access",
-				Browser = sp.GetRequiredService<WebAuthenticatorBrowser>(),
+        builder.Services.AddTransient<OidcClient>(sp =>
+            new OidcClient(new OidcClientOptions
+            {
+                Authority = "http://192.168.1.5:5001",
+                ClientId = "native",
+                RedirectUri = "theworkbook://authorize",
+                Scope = "openid profile api offline_access",
+                Browser = sp.GetRequiredService<WebAuthenticatorBrowser>(),
 
-				// When locally debugging
-				Policy = new Policy() { Discovery = new IdentityModel.Client.DiscoveryPolicy() { RequireHttps = false, LoopbackAddresses = loopbackAddresses } },
-			})
-		);
+                // When locally debugging
+                Policy = new Policy() { Discovery = new IdentityModel.Client.DiscoveryPolicy() { RequireHttps = false, LoopbackAddresses = loopbackAddresses } },
+            })
+        );
 
-		return builder.Build();
-	}
+        // When using https://auth.theworkbook.ie.
+        //builder.Services.AddTransient<OidcClient>(sp =>
+        //   new OidcClient(new OidcClientOptions
+        //   {
+        //       Authority = "https://auth.theworkbook.ie",
+        //       ClientId = "native",
+        //       RedirectUri = "theworkbook://connect/authorize",
+        //       Scope = "openid profile api offline_access",
+        //       Browser = sp.GetRequiredService<WebAuthenticatorBrowser>()
+        //   })
+        //);
 
-	private static void ConfigureConfiguration(MauiAppBuilder builder)
-	{
-		var assembly = typeof(App).GetTypeInfo().Assembly;
-		builder.Configuration.AddJsonFile(new EmbeddedFileProvider(assembly), "appsettings.json", optional: false, false);
-	}
+        return builder.Build();
+    }
+
+    private static void ConfigureConfiguration(MauiAppBuilder builder)
+    {
+        var assembly = typeof(App).GetTypeInfo().Assembly;
+        builder.Configuration.AddJsonFile(new EmbeddedFileProvider(assembly), "appsettings.json", optional: false, false);
+    }
 }
